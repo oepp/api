d537445430abdaeb92c3e399b2fd47eac136c191
Nedret Çelik <nedretcelik97@gmail.com>
2020-04-07 01:06:46 +0300

    Integrated sending email with sendgrid.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const session = require("express-session");
 const app = express();
+require("dotenv").config();
 const cors = require("cors");
 const userRoutes = require("./routes/user");
 const port = 3001;
