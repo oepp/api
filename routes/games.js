44ac743a68db7ef90954459718553d92fe70d386
AlpYksl <56409177+AlpYksl@users.noreply.github.com>
2020-05-06 14:36:47 +0300

    Revert "Revert "add game""
    This reverts commit d7cfaf1bb611d865de200324c0668ad6c1a204a3.

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const mysql = require('mysql');
+const sgMail = require('@sendgrid/mail');
+const mailgun = require("mailgun-js");
+const DOMAIN = process.env.DOMAIN_KEY;
+const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN});
 const connection = mysql.createPool({
     connectionLimit: 100,
     host: process.env.MYSQL_HOST,
@@ -46,4 +50,43 @@ router.get('/',(req,res)=>{
              }
             });
           });
+       
+          router.post('/addGame', function(req,res) {
+            const data = req.body;
+            if(data.GameTitle !== "" && data.GameDescription !== "" && data.GameImage !== null && data.CategoryID !== 0 ){
+                    connection.query('SELECT * FROM GAMES WHERE GameTitle = ? AND GameDescription = ?', [data.GameTitle, data.GameDescription], function(error,result,fields){
+                        if(result.length>0) {
+                            res.status(200).json({ status: 'error', message: "Same name and description exsist"});
+                        }else{
+                            const sql = "INSERT INTO GAMES (GameTitle, GameDescription, GameImage, CategoryID) VALUES ?";
+                            const values = [
+                                [data.GameTitle, data.GameDescription, data.GameImage, data.CategoryID]
+                            ];
+                            console.log("Inserting games.");
+                            connection.query(sql, [values], async function (err, result) {
+                                if (err){
+                                    console.log(err);
+                                }
+        
+                                const emailMsg = {
+                                    from: "OEPP <postmaster@sandboxb035355204c840d887be78db5f2d0bc2.mailgun.org>",
+                                    to: data.email,
+                                    subject: "Thanks for Adding game in our storage.",
+                                    text: "Your game succesfully added."
+                            };
+                                mg.messages().send(emailMsg, function (error, body) {
+                                    console.log(body);
+                                });
+                                console.log("Number of records inserted: " + result.affectedRows);
+                                res.status(200).json({ status: 'success', message: "Inserted Data!"});
+                            });
+                        };
+                    });
+                
+            }else{
+                res.status(200).json({
+                    status: "error"
+                });
+            }
+        });
     module.exports = router;
