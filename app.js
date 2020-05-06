c64861cb33e4f93a3b752d97abe89be473a88dde
AlpYksl <56409177+AlpYksl@users.noreply.github.com>
2020-05-06 14:57:34 +0300

    Revert "Merge branch 'master' of https://github.com/oepp/api"
    This reverts commit 21c5b8d567db67c8cf934133cea4429e47db4c5c, reversing
    changes made to 8453b3356d3aea46526f785cfe57fbd99dcdf4a8.

--- app.js
+++ app.js
@@ -10,40 +10,20 @@ const categoryRoutes = require("./routes/category");
 const gameRoute = require('./routes/games');
 const contentRoute = require('./routes/contents');
 const port = 3001;
-const MySQLStore = require('express-mysql-session')(session);
 
-var storeOptions = {
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-};
- 
-var sessionStore = new MySQLStore(storeOptions);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-const corsOptions = {
-    origin: process.env.APP_URL,
-    credentials: true
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(session({ 
-    key:'oepp2020project',
-    secret: 'somerandonstuffsoepp2020',
-    name: 'oepp2020project',
-    saveUninitialized: false,
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
     resave: false, 
-    rolling: true,
-    store: sessionStore,
-    cookie: { 
-        httpOnly: true,
-        expires: 48000
-    } 
+    saveUninitialized: false, 
+    cookie: { expires: 6000000 } 
 }));
 
 app.use("/user", userRoutes);
